7414c080f1419ce0290e40422c8384cf299c534f
Anh Thư <58073397+ATrestRV@users.noreply.github.com>
2022-05-23 22:20:55 +0700

    Update ThreeSum.java

--- ThreeSum.java
+++ ThreeSum.java
@@ -11,21 +11,23 @@ public class ThreeSum {
 }
 class Solution {
     public static List<List<Integer>> threeSum(int[] nums) {
+        // Idea: Loop over the array, group one element with pair of twoSum -> Using the twoSum 
+        // problem approach
         Arrays.sort(nums);
         List<List<Integer>> list = new ArrayList<>();
         int n = nums.length;
         if (n<3) return list;
-        for (int i=0; i<n; i++) {
-            if (i>0 && nums[i] == nums[i-1]) continue;
+        for (int i=0; i<n; i++) { // loop over the array
+            if (i>0 && nums[i] == nums[i-1]) continue;  // avoid dupplicates 
             int left = i+1;
             int right = n-1;
-            while (left<right) {
+            while (left<right) { // Check all of the possibilities of one element ( different twoSum pairs)
                 int sum = nums[left]+nums[right]+nums[i];
                 if (sum==0) {
                     ArrayList<Integer> arrList = new ArrayList<>();
                     list.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     right--;
-                    while (nums[right]==nums[right+1] && left<right) right--;
+                    while (nums[right]==nums[right+1] && left<right) right--;  // Decrement the right pointer and check for dupplicates
                 } else if (sum>0) {
                     right--;
                 } else {
