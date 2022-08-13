354270a8a10a9c8af37f669a909a4ca0c65567f8
Anh Thư <58073397+aestheticVelvet@users.noreply.github.com>
2022-08-13 23:11:45 +0700

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -14,15 +14,15 @@ public class TwoSum {
     }
 
     public static int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
         HashMap<Integer,Integer> map = new HashMap<>();
+        int[] res = new int[2];
         for (int i=0; i<nums.length; i++) {
             if (map.containsKey(target-nums[i])) {
                 res[0] = map.get(target-nums[i]);
                 res[1] = i;
-            } else {
-                map.put(nums[i],i);
-            }
+                return res;
+            } 
+            map.put(nums[i],i);
         }
         return res;
     }
